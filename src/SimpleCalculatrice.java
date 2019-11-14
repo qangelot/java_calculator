ffa2f4a29da8353eadb3276012d5aa849560e56d
qangelot <57401552+qangelot@users.noreply.github.com>
2019-11-14 10:28:36 +0100

    Initial commit java

---
+++ src/SimpleCalculatrice.java
@@ -0,0 +1,11 @@
+
+
+import javax.swing.JFrame;
+import javax.swing.JButton;
+
+public class SimpleCalculatrice {
+
+	JButton monBouton = new JButton();
+	JFrame monCadre = new JFrame();
+
+}
---
+++ src/javaHelloWorld/HelloWorld.java
@@ -0,0 +1,22 @@
+//package javaHelloWorld;
+//
+//import java.util.Scanner; // récupérer ce que l'on tape au clavier...
+//
+//public class HelloWorld {
+//
+//	public static void main(String[] args) { // void : on ne retournera rien dans cette fonction, on peut mettre int ou string, c'est le type de retour...
+//		
+//		double nb1 = 80_000;	//on définit la variable en lui donnant le bon type, on peut mettre _ pour la lisibilité...
+//		double nb2 = 3_000;
+//		double result;
+//		result = nb1-nb2;
+//		System.out.println("\nHELLO WORLD!");   // main est une fonction type de java, que l'on type avec string et args est la variable, en java on type tout...
+//		System.out.println(result);
+//		
+//		Scanner sc = new Scanner(System.in); //on créer un type scanner (comme les double ou int...) et ensuite on instancie sc avec la classe scanner...
+//		System.out.println("Saisir texte");
+//		String response = sc.nextLine();  // un type string pour response et sc permet de retenir ce que l'on a taper...
+//		System.out.println("Vous avez saisi : " + response);
+//	}
+//
+//}
